b4b224a4f6254fc13751262338b81d83f2e2e47c
unknown <kinekarpallavi21@gmail.com>
2022-03-07 21:18:25 +0530

    Check Employee is Present or absent

--- EmployeeComutation/EmployeeComutation/Program.cs
+++ EmployeeComutation/EmployeeComutation/Program.cs
@@ -1,2 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to EmployeeWage");
+
+class Employee
+{
+    static void Main(String[] args)
+    {
+        int Full_time = 1;//constant
+        Random random = new Random();//comutaion
+        int empCheck = random.Next(0, 2);
+        if (empCheck == Full_time)
+        {
+            Console.WriteLine("Employee is Present");
+        }
+        else
+        {
+            Console.WriteLine("Employee is Absent");
+        }
+
+    }
+}
+
