11e8bc5422f285a241ad79e5518fb72730652133
unknown <kinekarpallavi21@gmail.com>
2022-03-07 22:29:17 +0530

    Employee is full time then calculate employee wage

--- EmployeeComutation/EmployeeComutation/Program.cs
+++ EmployeeComutation/EmployeeComutation/Program.cs
@@ -5,17 +5,23 @@ class Employee
     static void Main(String[] args)
     {
         int Full_time = 1;//constant
+        int Emp_rate_per_hrs = 20;//varible
+        int empHrs = 0;
+        int empWage = 0;
         Random random = new Random();//comutaion
         int empCheck = random.Next(0, 2);
         if (empCheck == Full_time)
         {
-            Console.WriteLine("Employee is Present");
+            empHrs = 8;//Console.WriteLine("Employee is Present");
         }
         else
         {
-            Console.WriteLine("Employee is Absent");
+            empHrs = 0; //Console.WriteLine("Employee is Absent");
         }
-
+        empWage = empHrs * Emp_rate_per_hrs;
+        Console.WriteLine("Emp Wage:" + empWage);
     }
 }
+
+
 
