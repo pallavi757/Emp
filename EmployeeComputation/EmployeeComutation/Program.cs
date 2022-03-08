aef787c872b138dfb82a1dca11c99035bcecd78c
unknown <kinekarpallavi21@gmail.com>
2022-03-08 11:28:28 +0530

    Switch case example

---
+++ EmployeeComputation/EmployeeComutation/Program.cs
@@ -0,0 +1,80 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//class Employee
+//{
+//    static void Main(String[] args)
+//    {
+//        int Present = 1;
+//        int Full_time = 1;//constant
+//        int Emp_rate_per_hrs = 20;//varible
+//        int empHrs = 0;
+//        int empWage = 0;
+//        Random random = new Random();//comutaion
+//        Random ram = new Random();
+
+//        int empCheck = random.Next(0, 3);
+//        int empTime = ram.Next(0, 3);
+//        if (empCheck == Present)
+//        {
+//            if (empTime == Full_time)
+//            {
+//                Console.WriteLine("Employee is Present");
+//                empHrs = 8;
+//            }
+//            else
+//            {
+//                Console.WriteLine("Employee is Part time present");
+//                empHrs = 4;
+//            }
+//        }
+//        else
+//        {
+//            Console.WriteLine("Employee is absent  ");
+//            empHrs = 0;
+//        }
+
+//       //Console.WriteLine("Emloyee is absent");
+//        empWage = empHrs * Emp_rate_per_hrs;
+//        Console.WriteLine("Emp Wage:" + empWage);
+//    }
+//}
+using System;
+namespace EmployeeComputation
+{
+    //UC4
+    class Employee
+    {
+        
+        public const int Part_time = 1;
+        public const int Full_time = 2;
+        public const int Emp_rate_per_hrs = 20;
+
+        static void Main(string[] args)
+        {
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+            switch (empCheck)
+            {
+                case Part_time:
+                    empHrs = 4;
+                    Console.WriteLine("Employee is Present");
+                    Console.WriteLine("Employee is Part time");
+                    break;
+                case Full_time:
+                    empHrs = 8;
+                    Console.WriteLine("Employee is Present");
+                    Console.WriteLine("Employee is Full time");
+                    break;
+                default:
+                    empHrs = 0;
+                    Console.WriteLine("Employee is Absent");
+                    break;
+            }
+            empWage = (Emp_rate_per_hrs * empHrs);
+            Console.WriteLine("Daily emp wage: " + empWage);
+        }
+    }
+}
+
