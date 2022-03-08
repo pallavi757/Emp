070af98d0b9913bbbebfb168943ff5cf7aaa2afe
unknown <kinekarpallavi21@gmail.com>
2022-03-08 12:03:41 +0530

    Using ForLoop calculate total wage

--- EmployeeComputation/EmployeeComutation/Program.cs
+++ EmployeeComputation/EmployeeComutation/Program.cs
@@ -1,80 +1,51 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-//class Employee
-//{
-//    static void Main(String[] args)
-//    {
-//        int Present = 1;
-//        int Full_time = 1;//constant
-//        int Emp_rate_per_hrs = 20;//varible
-//        int empHrs = 0;
-//        int empWage = 0;
-//        Random random = new Random();//comutaion
-//        Random ram = new Random();
-
-//        int empCheck = random.Next(0, 3);
-//        int empTime = ram.Next(0, 3);
-//        if (empCheck == Present)
-//        {
-//            if (empTime == Full_time)
-//            {
-//                Console.WriteLine("Employee is Present");
-//                empHrs = 8;
-//            }
-//            else
-//            {
-//                Console.WriteLine("Employee is Part time present");
-//                empHrs = 4;
-//            }
-//        }
-//        else
-//        {
-//            Console.WriteLine("Employee is absent  ");
-//            empHrs = 0;
-//        }
-
-//       //Console.WriteLine("Emloyee is absent");
-//        empWage = empHrs * Emp_rate_per_hrs;
-//        Console.WriteLine("Emp Wage:" + empWage);
-//    }
-//}
 using System;
 namespace EmployeeComputation
 {
-    //UC4
-    class Employee
-    {
-        
-        public const int Part_time = 1;
-        public const int Full_time = 2;
-        public const int Emp_rate_per_hrs = 20;
-
-        static void Main(string[] args)
+    //UC6
+        class Employee
         {
-            int empHrs = 0;
-            int empWage = 0;
-            Random random = new Random();
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
+            
+            public const int Part_time = 1;
+            public const int Full_time = 2;
+            public const int Emp_rate_per_hrs = 20;
+            public const int Num_Of_Working_Days = 20;
+
+            static void Main(string[] args)
             {
-                case Part_time:
-                    empHrs = 4;
-                    Console.WriteLine("Employee is Present");
-                    Console.WriteLine("Employee is Part time");
-                    break;
-                case Full_time:
-                    empHrs = 8;
-                    Console.WriteLine("Employee is Present");
-                    Console.WriteLine("Employee is Full time");
-                    break;
-                default:
-                    empHrs = 0;
-                    Console.WriteLine("Employee is Absent");
-                    break;
+               // Console.WriteLine("Welcome to Employee Wage Computation Program");
+                int empHrs = 0;
+                int empWage = 0;
+                int totalEmpwage = 0;
+                for (int day = 0; day < Num_Of_Working_Days; day++)
+                {
+                    Random random = new Random();
+                    int empCheck = random.Next(0, 3);
+                    switch (empCheck)
+                    {
+                        case Part_time:
+                            empHrs = 4;
+                            Console.WriteLine("Employee is Present");
+                            Console.WriteLine("Employee is Part time");
+                            break;
+                        case Full_time:
+                            empHrs = 8;
+                            Console.WriteLine("Employee is Present");
+                            Console.WriteLine("Employee is Full time");
+                            break;
+                        default:
+                            empHrs = 0;
+                            Console.WriteLine("Employee is Absent");
+                            break;
+                    }
+                    empWage = (Emp_rate_per_hrs * empHrs);
+                    totalEmpwage += empWage;
+                    Console.WriteLine("Daily emp wage: " + empWage);
+                }
+                Console.WriteLine("Total Emp Wage: " + totalEmpwage);
             }
-            empWage = (Emp_rate_per_hrs * empHrs);
-            Console.WriteLine("Daily emp wage: " + empWage);
         }
     }
-}
+
 
